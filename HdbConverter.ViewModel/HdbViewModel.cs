be7eb8f3ddc5ad2f6746cd0d18f53b6ec1778f70
Martin Srdoš <martin.srdos@gmail.com>
2025-08-01 19:16:13 +0200

    HdbViewModel - FIX - Preventing cyclic calculations using _isUpdating field.

--- HdbConverter.ViewModel/HdbViewModel.cs
+++ HdbConverter.ViewModel/HdbViewModel.cs
@@ -10,6 +10,7 @@ public partial class HdbViewModel : ObservableObject
 {
 
     private readonly Converter _converter;
+    private bool _isUpdating = false;
 
     [ObservableProperty]
     private string _bin = string.Empty;
@@ -30,6 +31,10 @@ public partial class HdbViewModel : ObservableObject
 
     partial void OnBinChanged(string value)
     {
+        if (_isUpdating) return;
+        
+        _isUpdating = true;
+
         if (string.IsNullOrWhiteSpace(value))
         {
             Dec = Hex = string.Empty;
@@ -37,10 +42,14 @@ public partial class HdbViewModel : ObservableObject
         }
         Dec = _converter.Bin2Dec(value);
         Hex = _converter.Bin2Hex(value);
+
+        _isUpdating = false;
     }
 
     partial void OnDecChanged(string value)
     {
+        if (_isUpdating) return;
+        _isUpdating = true;
         if (string.IsNullOrWhiteSpace(value))
         {
             Bin = Hex = string.Empty;
@@ -48,10 +57,13 @@ public partial class HdbViewModel : ObservableObject
         }
         Bin = _converter.Dec2Bin(value);
         Hex = _converter.Dec2Hex(value);
+        _isUpdating = false;
     }
 
     partial void OnHexChanged(string value)
     {
+        if (_isUpdating) return;
+        _isUpdating = true;
         if (string.IsNullOrWhiteSpace(value))
         {
             Bin = Dec = string.Empty;
@@ -59,6 +71,7 @@ public partial class HdbViewModel : ObservableObject
         }
         Bin = _converter.Hex2Bin(value);
         Dec = _converter.Hex2Dec(value);
+        _isUpdating = false;
     }
 
 }
